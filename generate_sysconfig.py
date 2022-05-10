3e3bb7f43aefdc43cc16bf534180637be841de22
messense <messense@icloud.com>
2022-05-10 18:39:26 +0800

    Test

--- generate_sysconfig.py
+++ generate_sysconfig.py
@@ -18,38 +18,56 @@ OS_ALIAS = {
 
 
 def main():
-    machine = platform.machine()
+    machine = platform.machine().lower()
     arch = ARCH_ALIAS.get(machine, machine)
     plat = platform.system().lower()
     os = OS_ALIAS.get(plat, plat)
+    if os == "windows":
+        py_arches = ["64", "32"]
+    else:
+        py_arches = ["64"]
 
     well_known = defaultdict(list)
     for impl in PY_IMPLS:
         for minor in range(MIN_MINOR_VER, MAX_MINOR_VER + 1):
-            executable = f"{impl}3.{minor}"
-            command = [executable, "get_interpreter_metadata.py"]
-            try:
-                metadata = subprocess.check_output(command).decode().strip()
-            except FileNotFoundError:
-                print(f"{executable} not found, skipped.", file=sys.stderr)
-                continue
-            except subprocess.CalledProcessError as exc:
-                print(exc.output, file=sys.stderr)
-                continue
-            metadata = json.loads(metadata.splitlines()[-1])
-            for key in ["system", "platform"]:
-                metadata.pop(key, None)
-            well_known[arch].append(metadata)
+            for py_arch in py_arches:
+                if os == "windows":
+                    if impl == "pypy":
+                        # py.exe doesn't support finding pypy
+                        continue
+                    command = [
+                        "py",
+                        f"-3.{minor}-{py_arch}",
+                        "get_interpreter_metadata.py",
+                    ]
+                    if py_arch == "32":
+                        arch = "i686"
+                    else:
+                        arch = "x86_64"
+                else:
+                    command = [f"{impl}3.{minor}", "get_interpreter_metadata.py"]
+                try:
+                    metadata = subprocess.check_output(command).decode().strip()
+                except FileNotFoundError:
+                    print(f"{impl}3.{minor} not found, skipped.", file=sys.stderr)
+                    continue
+                except subprocess.CalledProcessError as exc:
+                    print(exc.output.decode(), file=sys.stderr)
+                    continue
+                metadata = json.loads(metadata.splitlines()[-1])
+                for key in ["system", "platform"]:
+                    metadata.pop(key, None)
+                well_known[arch].append(metadata)
 
-            # on macOS the sysconfig we care about are the same on x86_64 and aarch64
-            # so we can just duplicate it
-            #
-            # Note that pypy doesn't support aarch64 macOS yet
-            if os == "macos" and impl != "pypy":
-                if arch == "x86_64":
-                    well_known["aarch64"].append(metadata)
-                elif arch == "aarch64":
-                    well_known["x86_64"].append(metadata)
+                # on macOS the sysconfig we care about are the same on x86_64 and aarch64
+                # so we can just duplicate it
+                #
+                # Note that pypy doesn't support aarch64 macOS yet
+                if os == "macos" and impl != "pypy":
+                    if arch == "x86_64":
+                        well_known["aarch64"].append(metadata)
+                    elif arch == "aarch64":
+                        well_known["x86_64"].append(metadata)
 
     with open(f"sysconfig-{os}.json", "w") as f:
         f.write(json.dumps(well_known, indent=2))

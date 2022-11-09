6729361bb518707497243198d8ce42eb60a7b3c3
Marcel Bargull <marcel.bargull@udo.edu>
2022-11-09 23:26:44 +0100

    Use same paths from manylinux in conda-forge image

--- generate_manylinux.py
+++ generate_manylinux.py
@@ -92,44 +92,42 @@ def armv7l():
     return sysconfig
 
 
-def conda_forge_ppc64le():
-    # PyPy is not available on ppc64le via manylinux; use conda-forge instead
-    docker_image = "conda-forge-ppc64le"
-    pythons = [
-        "pypy3.7",
-        "pypy3.8",
-        "pypy3.9",
-    ]
+def condaforge_ppc64le():
+    sysconfig = defaultdict(list)
     cwd = os.getcwd()
-    sysconfig = {"ppc64le": []}
-    for python in pythons:
-        command = [
-            "docker",
-            "run",
-            "--rm",
-            "-v",
-            f"{cwd}:/io",
-            "-w",
-            "/io",
-            docker_image,
-            f"/opt/{python}/bin/python",
-            "/io/get_interpreter_metadata.py",
-        ]
-        try:
-            metadata = subprocess.check_output(command).decode().strip()
-        except subprocess.CalledProcessError as exc:
-            print(exc.output, file=sys.stderr)
-            raise
-        metadata = json.loads(metadata.splitlines()[-1])
-        for key in ["system", "platform"]:
-            metadata.pop(key, None)
-        sysconfig["ppc64le"].append(metadata)
+    # PyPy is not available on ppc64le via manylinux; use conda-forge instead
+    for arch in ["ppc64le"]:
+        docker_image = f"condaforge_{arch}"
+        for ver in PY_VERS:
+            if not ver.startswith("pp"):
+                continue
+            command = [
+                "docker",
+                "run",
+                "--rm",
+                "-v",
+                f"{cwd}:/io",
+                "-w",
+                "/io",
+                docker_image,
+                f"/opt/python/{ver}/bin/python",
+                "/io/get_interpreter_metadata.py",
+            ]
+            try:
+                metadata = subprocess.check_output(command).decode().strip()
+            except subprocess.CalledProcessError as exc:
+                print(exc.output, file=sys.stderr)
+                raise
+            metadata = json.loads(metadata.splitlines()[-1])
+            for key in ["system", "platform"]:
+                metadata.pop(key, None)
+            sysconfig[arch].append(metadata)
     return sysconfig
 
 
 def main():
     well_known = manylinux()
-    for sysconfig in (armv7l(), conda_forge_ppc64le()):
+    for sysconfig in (armv7l(), condaforge_ppc64le()):
         for arch, metadatas in sysconfig.items():
             well_known[arch].extend(metadatas)
     with open("sysconfig-linux.json", "w") as f:
